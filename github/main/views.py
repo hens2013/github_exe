110e0ae05730caa62b6775ea533c754a08e79b0c
hens2013 <hens2013@gmail.com>
2023-05-16 20:35:34 +0300

    adding payload code

--- github/main/views.py
+++ github/main/views.py
@@ -5,20 +5,21 @@ from .models import PullRequest
 
 
 def index(request):
-    if request.method == 'POST':
-        # Process the webhook payload
-        payload = request.POST
-
-        # Extract pull request details from the payload
-        title = payload['pull_request']['title']
-        url = payload['pull_request']['url']
-
-        # Save the pull request details to the database
-        pull_request = PullRequest.objects.create(title=title, url=url)
-
-        # Return a success response
-        return HttpResponse('Webhook received and processed successfully.')
-    else:
-        # Return a 405 Method Not Allowed response for other HTTP methods
-        return HttpResponse("tech with team")
+    print(request)
+    # if request.method == 'GET':
+    #     # Process the webhook payload
+    #     payload = request.POST
+    #
+    #     # Extract pull request details from the payload
+    #     title = payload['pull_request']['title']
+    #     url = payload['pull_request']['url']
+    #
+    #     # Save the pull request details to the database
+    #     pull_request = PullRequest.objects.create(title=title, url=url)
+    #
+    #     # Return a success response
+    #     return HttpResponse('Webhook received and processed successfully.')
+    # else:
+    #     # Return a 405 Method Not Allowed response for other HTTP methods
+    return HttpResponse("tech with team")
 
